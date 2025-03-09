ef9eeafb85f208cf65214e9dc56103f21998826c
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-03-09 15:49:39 +0100

    Changed version code for android publish

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -134,7 +134,7 @@ android {
         applicationId = "com.tecknobit.brownie"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
     }
     packaging {
